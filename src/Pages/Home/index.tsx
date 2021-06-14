c04e29a26f85d30165df9a6187a094a381fb95cc
Santiago Gonzalez <santiago@alimx.mx>
2021-06-14 14:49:32 -0500

    Se creo la vista de Home con inputs para login

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,54 @@
-import React,{FC,Fragment} from 'react';
-import './index.css'
+import React,{FC} from 'react';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import {Avatar,Button,CssBaseline,TextField,Typography,Container} from '@material-ui/core';
+
+import LoginStyles from './LoginStyles';
 
 const index: FC = () => {
+
+  const classes = LoginStyles();
   return(
-    <Fragment>
-      Home
-    </Fragment>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="User Name"
+            name="userName"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
   )
 }
 
