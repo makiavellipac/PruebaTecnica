5f73e86c6b4ae031bbba1b55478e8d02d28c27cf
Santiago Gonzalez <santiago@alimx.mx>
2021-06-14 18:17:29 -0500

    se configuro la dataGrid de empleados en la pagina de empĺoyees

--- src/Pages/Employees/index.tsx
+++ src/Pages/Employees/index.tsx
@@ -1,10 +1,30 @@
-import React,{FC,Fragment} from 'react';
-import './index.css'
+import React,{FC,Fragment, useContext, useEffect, useState,} from 'react';
+import { GridRowData } from '@material-ui/data-grid';
+import { Mycontext } from '../../Components/Context';
+import DataContainer from '../../Components/DataContainer';
+
+
+import EMPLOYES_SERVICE from '../../Services/axiosServices';
 
 const index: FC = () => {
+
+  const {userInfo} = useContext(Mycontext);
+  const {userName} = userInfo;
+
+  const [employess,setEmployess]=useState<GridRowData[]>([])
+
+  useEffect(()=>{
+    userName!==''?
+    EMPLOYES_SERVICE.getEmployes(userName).then((data)=>
+    {
+      setEmployess(data) 
+    }
+    ):null
+  },[])
+
   return(
     <Fragment>
-      Employes
+      <DataContainer rows={employess}/>
     </Fragment>
   )
 }
