3bfe46a82f1efb66b05c4769fba5087709ab22f5
Santiago Gonzalez <santiago@alimx.mx>
2021-06-14 15:23:05 -0500

    se crearon los types y la funcionalidad para controlar los inputs en el login

--- src/Components/Context.tsx
+++ src/Components/Context.tsx
@@ -7,19 +7,47 @@ const contextInit: ContexState = {
   userInfo:{
     userName:''
   },
+  login: {
+    userName:'',
+    password:''
+  },
   setUserName:() => {},
+  setLogin: () => {},
+  clearLogin:()=> {}
 }
 
 export const Mycontext = createContext<ContexState>(contextInit);
 
 const MyProvider: FC =({children}) => {
+
   const [userInfo, setUserInfo] = useState<{userName:String}>(contextInit.userInfo);
+  const [login, setLogi] = useState<{userName:String,password:String
+  }>(contextInit.login);
+
   const setUserName = (userName:String) => {
     setUserInfo({userName})
   }
 
+  const setLogin = (value:String,name:String)=>{
+    setLogi(
+      {
+        ...login,
+        [name.toString()]:value
+      }
+    )
+  }
+
+  const clearLogin = ()=>{
+    setLogi(
+      {
+        userName:'',
+        password:''
+      }
+    )
+  }
+
   return (
-    <Mycontext.Provider value={{userInfo,setUserName}}>
+    <Mycontext.Provider value={{userInfo,login,setUserName,setLogin,clearLogin}}>
       {children}
     </Mycontext.Provider>
   )
--- src/Types/types.ts
+++ src/Types/types.ts
@@ -2,5 +2,11 @@ export type ContexState = {
   userInfo:{
     userName:String
   },
+  login: {
+    userName:String,
+    password:String
+  };
   setUserName: (userName:String) => void;
+  setLogin: (value:String,name:String) => void;
+  clearLogin:()=>void
 }
