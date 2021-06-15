c7c2a5eda2a232667620aa91a060f72355d367e1
Santiago Gonzalez <santiago@alimx.mx>
2021-06-14 21:44:17 -0500

    se elimino el console.log

--- src/Services/loginServices.ts
+++ src/Services/loginServices.ts
@@ -3,7 +3,6 @@ import {createAuthToken} from '../Helpers/loginHelpers';
 
 const Myservice ={
   login:async (user:String)=>{
-     console.log(process.env.JWT_SECRET)
      const newToken= createAuthToken(user,200,process.env.JWT_SECRET,process.env.EXPIRES_IN)
            .then((token)=>localStorage.setItem('token',token.token))
      return newToken
