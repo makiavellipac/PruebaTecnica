2244cacb1ad25a97d4c0bdb937ca02a7683b192d
Santiago Gonzalez <santiago@alimx.mx>
2021-06-15 15:13:24 -0500

    se creo el evento onsubmit para subir la data

--- src/Components/FormEmployes.tsx
+++ src/Components/FormEmployes.tsx
@@ -1,11 +1,19 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState,FormEvent  } from 'react';
 import { TextField, Theme, createStyles, makeStyles, Button, } from '@material-ui/core';
+
+import EMPLOYES_SERVICE from '../Services/axiosServices';
+import dateParse  from '../Helpers/dateHelpers';
 
 
 type forms={
   name:string,
   last_name:string,
   birthday:string
+}
+
+type typeProps={
+  userName:String,
+  onClick:any
 }
 
 const useStyles = makeStyles((theme:Theme)=>
@@ -21,7 +29,7 @@ const useStyles = makeStyles((theme:Theme)=>
   })
 )
 
-const FormEmployes = () =>{
+const FormEmployes = (props:typeProps) =>{
 
   const [forms,setForms]=useState<forms>({
     name:'',
@@ -38,6 +46,17 @@ const FormEmployes = () =>{
     }
   }
 
+  const onSubmit = (e: FormEvent)=>{
+    if(Object.values(forms).find(value=>value==='')!==undefined){
+      alert('Completa todos los campos')
+    }
+    else{
+      EMPLOYES_SERVICE.saveEmployes({...forms,birthday:dateParse(forms.birthday)},props.userName).then(
+        data=>props.onClick()
+      )
+    }
+  }
+
   const classes=useStyles()
 
   return(
@@ -45,7 +64,7 @@ const FormEmployes = () =>{
       <TextField label='Nombre' name='name' variant='outlined' className={classes.textField} value={forms.name} onChange={handleChangue}/>
       <TextField label='Apellidos' name='last_name' variant='outlined' className={classes.textField} value={forms.last_name} onChange={handleChangue}/>
       <TextField name='birthday' variant='outlined' type='date' className={classes.textField} value={forms.birthday} onChange={handleChangue}/>
-      <Button color='primary' variant='contained' className={classes.textField}>Submit</Button>
+      <Button color='primary' variant='contained' className={classes.textField} onClick={onSubmit} >Submit</Button>
     </form>
   )
 }
